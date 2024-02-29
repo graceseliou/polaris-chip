2fe81eb1614cc4609032d69b973799820858b602
Grace Liou <gsl5185@psu.edu>
2024-02-29 15:41:01 -0500

    alert fin 1

--- src/alert-notification.js
+++ src/alert-notification.js
@@ -9,81 +9,184 @@ export class Alert extends LitElement {
     constructor() {
         super();
         this.state="";
-        this.icon="#";
         this.date="";
-        this.notification="";
+        this.message="";
 
-        this.open=true;
-        this.fancy=false;
+        this.opened=true;
+        if (localStorage.getItem('campus-alert-opened-state') == "false") {
+            this.opened=false;
+        }
+        
         this.sticky=false;
     }
 
     static get styles() {
         return css`
-            :host([sticky]).alert-wrapper {
-                position: sticky;
-                top: 0;
-                z-index: 1000;
-            }
-
-            :host {
-                display: inline-flex;
-            }
-
-            .alert-wrapper {
-                padding: 8px;
-                font-size: 16px;
-                color: black;
-                width: 100vw;
-            }
 
             .sticky {
                 position: sticky;
                 top: 0;
                 z-index: 100;
             }
+            
+            :host([date]){
+                color: black;
+            }
+
+            .alert-wrapper {
+                background-color: pink;
+                margin-bottom: 16px;
+                font-size: 16px;
+                color: black;
+                width: 100vw;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            /* .alert-wrapper-left,
+            .alert-wrapper-right {
+                display: flex;
+                align-items: center;
+            } */
+
+            .date {
+                font-size: 16px;
+                font-weight: bold;
+                color: black;
+            }
+
+            .close-button {
+                font-size: 16px;
+                border: none;
+                background: none;
+                margin-bottom: 40px;
+            }
+
+            .message-wrap {
+                background-color: white;
+                transform: skew(20deg);
+                height: 100%;
+                text-align: center;
+                width: 600px;
+                margin-left: 16px;
+                margin-right: 16px;
+                display: inline-flex;
+            }
 
             .icon {
-                display: inline-flex;
-                width: 200px;
+                width: 50px;
+                height: 40px;
+                transform: skew(-20deg);
+                margin-left: 20px;
+                margin-top: 25px;
             }
 
-            /* details summary {
-                background-color: orange;
+            .message {
+                font-size: 16px;
+                padding: 20px;
                 color: black;
-                text-align: center;
-                font-size: 32px;
-                padding: 16px;
+                transform: skew(-20deg);
             }
 
-            details[open] summary {
-                background-color: black;
-                color: white;
-                font-weight: bold;
+            .openedContainer{
+                height: 100%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
             }
-      
-            details div {
-                display: block;
-                background-color: lightblue;
-                color: white;
-                border: 2px solid black;
-                text-align: left;
-                padding: 8px;
-                height: 70px;
-                overflow: auto;
-            } */
+
+            .closedContainer {
+                display: flex;
+                margin-bottom: 8px;
+                align-items: center;
+                justify-content: center;
+                width: 100%;
+            }
+
+            .closed-toggle-button {
+                display: flex;
+                margin-bottom: 8px;
+                align-items: center;
+                justify-content: center;
+                padding: 10px;
+                font-size: 30px;
+                background-color: green;
+                width: 100%;
+            }
+
+            .closed-alert {
+                font-size: 25px;
+                font-family: sans-serif;
+                font-weight: bold;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100%;
+            }
+
+            .alert-wrapper,
+            .date,
+            .message,
+            .close-button {
+                font-family: sans-serif;
+            }
         `;
     }
 
-    openChanged(e) {
-        console.log(e.newState);
-        if (e.newState === "open") {
-          this.fancy = true;
+    toggleAlert() {
+        this.opened=!this.opened;
+        localStorage.setItem("campus-alert-opened-state", this.opened);
+    }
+
+    openedView(color, secondaryColor) {
+        return html `
+        <div class="alert-wrapper ${(this.sticky) ? "sticky" : ""}"  style="background-color:${secondaryColor}">
+            <div class="openedContainer">
+                <div class="alert-wrapper-left">
+                    <div class="date">${this.date}</div>
+                </div>
+                    <div class="message-wrap" style="background-color:${color}">
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <title>alert-circle-outline</title>
+                        <path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" />
+                    </svg>
+                    <p class="message">${this.message}</p>
+                    </div>
+                <div class="alert-wrapper-right">
+                    <button class="close-button" @click="${this.toggleAlert}">&times close</button>
+                </div>
+                <slot></slot>
+            </div>
+        </div>
+        `;
+    }
+
+    handleKeyDown(event) {
+        if(event.keyCode === 13) {
+            this.toggleAlert();
         }
-        else {
-          this.fancy = false;
-        }
-      }
+    }
+
+    closedView(color) {
+        return html `
+        <div class="closedContainer ${(this.sticky) ? "sticky" : ""}">
+            <div class="closed-toggle-button" style="background-color: ${color}" @click="${this.toggleAlert}" @keydown="${this.handleKeyDown}" tabindex="0">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="40">
+                    <title>alert-circle-outline</title>
+                    <path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" />
+                </svg>
+                <div class="closed-alert">
+                    <p>TEST CAMPUS ALERT</p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="40">
+                    <title>chevron-down</title>
+                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+                </div>
+            </div>
+        </div>
+        `;
+    }
 
     render() {
         let color = "white";
@@ -91,34 +194,28 @@ export class Alert extends LitElement {
         if(this.state === "warning") color = "yellow";
         if(this.state === "alert") color = "red";
 
-        return html`
-        <div class="alert-wrapper ${this.open ? "" : "closed"}" ?sticky="${this.sticky}" style="background-color:${color}">
-            <h1 class="state">${this.state}</h1>
-            <h1 class="notification">${this.notification}</h1>
-        </div>
+        let secondaryColor = "white";
+        if(this.state === "notice") secondaryColor = "blue";
+        if(this.state === "warning") secondaryColor = "orange";
+        if(this.state === "alert") secondaryColor = "pink";
 
-        <!-- <details ?open="${this.fancy}" @toggle="${this.openChanged}">
-                <summary>
-                    ${this.state}
-                </summary>
-                <div>
-                    <img src="${this.icon}" alt="icon" class="icon">
-                    <slot>${this.date}</slot>
-                    <slot>${this.information}</slot>
-                </div>
-            </details> -->
-        `;
+        if(this.state === "notice") 
+            this.message = "notice message";
+        if(this.state === "warning") 
+            this.message = "warning message";
+        if(this.state === "alert") 
+            this.message = "alert message";
+        
+        return (this.opened) ? this.openedView(color, secondaryColor) : this.closedView(color);
     }
 
     static get properties() {
         return {
             state: { type: String, reflect: true },
-            icon: { type: String },
             date: { type: String },
-            notification: { type: String },
+            message: { type: String },
 
-            open: { type: Boolean, reflect: true },
-            fancy: { type: Boolean, reflect: true },
+            opened: { type: Boolean, reflect: true },
             sticky: { type: Boolean },
         }
     }
