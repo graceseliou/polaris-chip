886879b25811486e0680f05db612e46099d431a1
Grace Liou <gsl5185@psu.edu>
2024-03-26 16:12:42 -0400

    2/26 oh changes

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -11,8 +11,10 @@ export class PartyUI extends DDD {
         super();
         this.save="";
         this.username="";
-        this.userArray=[""];
+        this.userArray=[];
         this.maxUsers=4;
+        this.tempUser="";
+        this.sidebar=false;
     }
 
     static get styles() {
@@ -23,20 +25,30 @@ export class PartyUI extends DDD {
           display: block;
         }
         
-        .my-div {
+        p{
+            margin: 0px;
+        }
+
+        /* .my-div {
           padding: var(-ddd-spacing-5);
           margin: var(--ddd-spacing-2) var(--ddd-spacing-0);
           color: var(--ddd-theme-default-keystoneYellow);
+        } */
+
+        .everything-wrapper {
+            display: inline-flex;
+            flex-direction: row;
+            padding: 8px;
+            margin: 8px;
         }
 
         .party-ui-wrapper {
             background-color: grey;
             padding: 20px;
-            width: 450px;
-            height: 420px;
+            width: 650px;
+            height: 470px;
             display: inline-block;
             overflow: auto;
-            margin-left: 200px;
             border: 2px solid white;
         }
         
@@ -55,7 +67,7 @@ export class PartyUI extends DDD {
 
         .characters-wrapper {
             padding: 20px;
-            width: 350px;
+            width: 600px;
             display: flex;
             margin: auto;
             margin-bottom: 10px;
@@ -71,26 +83,50 @@ export class PartyUI extends DDD {
         .characters-wrapper p {
             justify-content: center;
             align-items: center;
-            margin-top: 15px;
+            /* margin-top: 15px; */
         }
 
-        .end-button {
-            margin-right: 30px;
+        .users-wrapper {
+            background-color: lightblue;
+            border: 2px solid white;
+            display: flex;
+            /* flex-wrap: wrap; */
         }
 
-        .save-button {
-            margin-left: 30px;
+        .user-container {
+            background-color: grey;
+            margin: 10px;
+            padding: 10px;
+            border: 2px solid white;
+            /* position: relative; */
+        }
+
+        .delete-button {
+            margin-left: 22px;
         }
 
         .end-wrapper {
             background-color: lightblue;
-            padding: 5px;
+            padding: 20px;
             width: 330px;
             display: flex;
             margin: auto;
             justify-content: center;
             align-items: center;
             border: 2px solid white;
+        }
+
+        .sidebar-wrapper {
+            display: flex;
+            width: 200px;
+        }
+
+        .popUp {
+            display: flex;
+            flex-direction: column;
+            background-color: lightblue;
+            border: 2px solid white;
+            color: black;
         }
       `];
     }
@@ -102,62 +138,101 @@ export class PartyUI extends DDD {
             username: { type: String, reflect: true },
             userArray: { type: Array, reflect: true },
             maxUsers: { type: Number },
+            tempUser: { type: String }, 
+            sidebar: { type: Boolean, reflect: true },
         }
     }
 
     render() {
         return html`
-        <div class="party-ui-wrapper">
-            <div class="usernames-wrapper">
-                <input class="text-input" type="text" value=${this.username} @input=${this.updateName}>
-                <button id="add-user-button" @click="${this.addUser}" ?disabled="${this.userArray.length >= this.maxUsers}">Add User</button>
-            </div>
-            <div class="characters-wrapper">
-                <div class="users-wrapper"></div>
-                ${this.userArray.map(element =>
-                    this.characterView(element))}
-            </div>
-            <confetti-container id="confetti">
-                <div class="end-wrapper">
-                    <p class="save">${this.save}</p>
-                    <button class="end-button" @click="${this.deleteUser}">Delete</button>
-                    <button class="save-button" @click="${this.makeItRain}">Save</button>
+        <div class="everything-wrapper">
+            <div class="party-ui-wrapper">
+                <div class="usernames-wrapper">
+                    <input class="text-input" type="text" value=${this.username} @input=${this.updateName}>
+                    <button id="add-user-button" @click="${this.addUser}" ?disabled="${this.userArray.length >= this.maxUsers}">Add User</button>
                 </div>
-            </confetti-container>
+                <div class="characters-wrapper">
+                    <div class="users-wrapper">
+                        ${this.userArray.map(element =>
+                            this.characterView(element))}
+                    </div>
+                </div>
+                <confetti-container id="confetti">
+                    <div class="end-wrapper">
+                        <p class="save">${this.save}</p>
+                        <button class="save-button" @click="${this.makeItRain}" ?disabled="${this.userArray.length == 0}">Save</button>
+                    </div>
+                </confetti-container>
+            </div>
+            <div class="sidebar-wrapper" style="${(!this.sidebar) ? "display:none;" : ""}">
+                <div class = popUp>
+                    ${this.characterView(this.tempUser)}
+                </div>
+            </div>
         </div>
         `;
     }
 
     characterView(name) {
         return html `
-        <div>
-            <rpg-character id="rpg" hat="random" seed=${name} style="height: 100px; width: 100px;"></rpg-character>
-            <p></p>
-            <p>${name}</p>
+        <div class="user-container">
+            <div class="user-character">
+                <rpg-character id="rpg" hat="random" seed=${name} style="height: 100px; width: 100px;"></rpg-character>
+                <br>
+                <p>${name}</p>
+            <button class="delete-button" id="${name}" @click="${this.deleteUser}">Delete</button>
         </div>
         `;
     }
 
     updateName(event) {
-        this.username = event.target.value;
-        this.userArray[this.userArray.length - 1] = event.target.value;
+        this.tempUser = event.target.value;
+        // this.username = event.target.value;
+        // this.userArray[this.userArray.length - 1] = event.target.value;
     }
 
     addUser(event) {
         if(this.userArray.length < this.maxUsers) {
-            this.userArray[this.userArray.length - 1];
+            this.userArray.push(this.tempUser);
+            // this.userArray[this.userArray.length - 1];
             this.shadowRoot.querySelector('.text-input').value ="";
-            this.userArray.push("");
+            // this.userArray.push("");
+            this.sidebar = false;
+            setTimeout(() => { 
+                this.requestUpdate();
+            });
         }
     }
 
     deleteUser(event) {
-        if(this.userArray.length>0) {
-            this.userArray.pop();
-            setTimeout(() => {
-                this.requestUpdate();
-            });
+        let id = event.target.id;
+        let position = this.userArray.indexOf(id);
+        
+        this.userArray.splice(position, 1);
+
+        this.requestUpdate();
+    }
+
+    deleteUserButton(name) {
+        const index = this.userArray.indexOf(name);
+        if (index !== 0) {
+            this.userArray.splice(index, 1);
+            this.requestUpdate();
         }
+    }
+
+    handleInput(event) {
+        const inputValue = event.target.value;
+
+        if(inputValue.length > -1) {
+            this.sidebar = true;
+        }
+        else {
+            this.sidebar = false;
+        }
+
+        const sanitizedValue = inputValue.replace(/[^a-z0-9]/g, "");
+        event.target.value = sanitizedValue.splice(0, 10);
     }
 
     makeItRain() {
